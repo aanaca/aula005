51ddedc7743b63a66ec28d5a9d0fe622eda75e23
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-03-12 18:50:18 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,8 +14,11 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('Informe seu Nome:', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    nome        = StringField('Informe o seu nome'                  , validators=[DataRequired()])
+    sobrenome   = StringField('Informe o seu sobrenome:'            , validators=[DataRequired()])
+    instituicao = StringField('Informe a sua Instituição de ensino:', validators=[DataRequired()])
+    disciplina  = SelectField('Informe a sua disciplina:'           , choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de projetos', 'Gestão de projetos')])
+    submit      = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -29,11 +33,24 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    remote_addr = request.remote_addr;
+    remote_host = request.host;
+    form        = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
+        old_name = session.get('nome')
+        if old_name is not None and old_name != form.nome.data:
+            flash('Você alterou o seu nome!')
+        session['name']        = form.nome.data
+        session['sobrenome']   = form.sobrenome.data
+        session['instituicao'] = form.instituicao.data
+        session['disciplina']  = form.disciplina.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                            current_time = datetime.utcnow(),
+                            form         = form,
+                            nome         = session.get('name'),
+                            sobrenome    = session.get('sobrenome'),
+                            instituicao  = session.get('instituicao'),
+                            disciplina   = session.get('disciplina'),
+                            remote_addr  = remote_addr,
+                            remote_host  = remote_host)
